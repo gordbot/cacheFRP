c51934162ca2f2a351158f5cf908bb6010dc9315
gordbot <gordonbonnar@gmail.com>
2020-04-17 21:10:54 -0400

    Updated to take input as a comma-seperated file with the following form: DEPT,FRPURL The script then loops over every line in the entry and downloads the relevant FRP files

--- cacheFRP.sh
+++ cacheFRP.sh
@@ -12,50 +12,49 @@
 # BASEURL - The URL from the end of the protocol to just before the filenam
 # TODO: Calculate BASEURL dynamicaly
 
-# Config file passed as argument
-if [ "$#" -ne 1 ]
-then
-    echo "Usage: cacheFRP.sh file.conf"
-    exit 1
-fi
-source $1
+while IFS=, read -r dept url
+do
+    # OUTPUTDIR - Where to save fownloaded files
+    FRPURL=$url
+    DEPT=$dept
+    echo $FRPURL
+    echo $DEPT
+    OUTPUTDIR="/var/www/frp.policygeek.ca/public_html"
 
-# OUTPUTDIR - Where to save fownloaded files
-OUTPUTDIR="/var/www/frp.policygeek.ca/public_html"
+    # Prepare other variables from parameters
+    DOMAIN=$(echo $FRPURL|awk -F[/:] '{print $4}')
+    FILENAME="${FRPURL##*/}"
 
-# Prepare other variables from parameters
-DOMAIN=$(echo $FRPURL|awk -F[/:] '{print $4}')
-FILENAME="${FRPURL##*/}"
-
-# Append the date to the directory using the format 
-# YYYY-MM-DD
-DIRECTORY="${OUTPUTDIR}/${DEPT}/$(date "+%Y-%m-%d")"
+    # Append the date to the directory using the format 
+    # YYYY-MM-DD
+    DIRECTORY="${OUTPUTDIR}/${DEPT}/$(date "+%Y-%m-%d")"
 
 
-# If the directory doesnt exist
-if [ ! -d "$DIRECTORY" ]; then
-    # Make the local directory for the snapshot AND
-    # Grab a copy of the directory recursively, staying within canada.ca domain, not following parent links
-	mkdir -p $DIRECTORY && wget --recursive --no-clobber --page-requisites --html-extension --convert-links --restrict-file-names=windows --domains canada.ca --no-parent -P $DIRECTORY $FRPURL 
+    # If the directory doesnt exist
+    if [ ! -d "$DIRECTORY" ]; then
+        # Make the local directory for the snapshot AND
+        # Grab a copy of the directory recursively, staying within canada.ca domain, not following parent links
+        mkdir -p $DIRECTORY && wget --recursive --no-clobber --page-requisites --html-extension --convert-links --restrict-file-names=windows --domains canada.ca --no-parent -P $DIRECTORY $FRPURL 
 
-    # When wget grabs all of the files, they are stored in the full directory structure.
-    # To keep things small and easy to work with, we use FRPURL to create a $BASEURL which
-    # can be used to move all the files higher in the directory.  
-    BASEURL=$(echo ${FRPURL} | sed "s|https:/\(.*\)/.*|\1/|") 
+        # When wget grabs all of the files, they are stored in the full directory structure.
+        # To keep things small and easy to work with, we use FRPURL to create a $BASEURL which
+        # can be used to move all the files higher in the directory.  
+        BASEURL=$(echo ${FRPURL} | sed "s|https:/\(.*\)/.*|\1/|") 
 
-    # Move the files from the deep file structure up to the main local directory
-	mv "${DIRECTORY}${BASEURL}"/* "${DIRECTORY}/"
+        # Move the files from the deep file structure up to the main local directory
+        mv "${DIRECTORY}${BASEURL}"/* "${DIRECTORY}/"
 
-    # Remove the base domain, often but not always www.canada.ca directory
-	rm -r "${DIRECTORY}/${DOMAIN}"
+        # Remove the base domain, often but not always www.canada.ca directory
+        rm -r "${DIRECTORY}/${DOMAIN}"
 
-    # To facilitate browsing archives, rename the main page to index.html
-    mv "${DIRECTORY}/${FILENAME}" "${DIRECTORY}/index.html"
+        # To facilitate browsing archives, rename the main page to index.html
+        mv "${DIRECTORY}/${FILENAME}" "${DIRECTORY}/index.html"
 
-    # Correct anchor references to point to current page
-    sed -i "s/${FILENAME}//" "${DIRECTORY}/index.html"
+        # Correct anchor references to point to current page
+        sed -i "s/${FILENAME}//" "${DIRECTORY}/index.html"
 
-    # Replace the stylesheet path with the local stylesheet store
-    # TODO: Abstract or parametrize how we handle the stylesheet
-    sed -i "s|href=\".*/etc/designs/canada/wet-boew/css/theme.min.css|href=\"../../styles/theme.min.css|" "${DIRECTORY}/index.html"
-fi
+        # Replace the stylesheet path with the local stylesheet store
+        # TODO: Abstract or parametrize how we handle the stylesheet
+        sed -i "s|href=\".*/etc/designs/canada/wet-boew/css/theme.min.css|href=\"../../styles/theme.min.css|" "${DIRECTORY}/index.html"
+    fi
+done < frpurls.csv
